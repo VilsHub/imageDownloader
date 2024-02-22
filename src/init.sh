df6ca5e46234704d18796e623187f64a4ae02fdc
starlyvil <starlyvil@gmail.com>
2024-02-22 14:57:55 +0100

    Fixed issue with partial environment setup

--- src/init.sh
+++ src/init.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-dir="/tmp/airGapTempFiles"
+dir="/tmp/airGapTempFiles/tempOutput"
 configsDir="$dir/configs"
 imageDir="$dir/images"
 trackDir="$dir/track"
+dockerImageOutputDir="$dir/tempOutput"
 
 if [ ! -d $dir ]; then
     # Directory does not exist
-    mkdir -p $configsDir $imageDir $trackDir
+    mkdir -p $configsDir $imageDir $trackDir $dockerImageOutputDir
 fi
